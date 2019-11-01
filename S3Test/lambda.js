667993495785b7deab55f96fbe4fbaa4c4239c47
zgma-test <uditharosha+zgma@gmail.com>
2019-11-01 10:55:02 +0530

    dd

--- S3Test/lambda.js
+++ S3Test/lambda.js
@@ -1,5 +1,29 @@
-exports.handler = function(event, context, callback) {
+let AWS = require('aws-sdk');
+const rekognition = new AWS.Rekognition({ apiVersion: '2016-06-27' });
+
+exports.handler = function (event, context, callback) {
 
     console.log(event);
-    callback(null, {"message": "Successfully executed"});
+    let s3Obj = event.Records[0].s3;
+
+    let params = {
+        Image: {
+            S3Object: {
+                Bucket: s3Obj.bucket.name,
+                Name: s3Obj.object.key
+            }
+        },
+        MaxLabels: 10
+    };
+    rekognition.detectLabels(params).promise()
+        .then(data => {
+            console.log(data);
+            callback(null, data);
+        })
+        .catch(err => {
+            console.log(err);
+            callback(err);
+        });
+
+    
 }
